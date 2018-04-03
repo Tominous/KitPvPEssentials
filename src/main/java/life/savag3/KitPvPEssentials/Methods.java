f7700f4de0d1ce9755d288dca2564093dcba3d50
Savag3Life <unknowngaming878@gmail.com>
2018-04-03 12:25:26 -0300

    Updated Placeholder support, random numbers

--- src/main/java/life/savag3/KitPvPEssentials/Methods.java
+++ src/main/java/life/savag3/KitPvPEssentials/Methods.java
@@ -9,16 +9,14 @@ public class Methods {
     public static String color(String msg) {
         return ChatColor.translateAlternateColorCodes('&', msg);
     }
-
-    public static String placeholders(String msg, Player p, Player k) {
-
-        if (msg.contains("%Player%")) {
-            msg.replaceAll("%Player%", p.getName());
-        }
-        if (msg.contains("%Killer%")) {
-            msg.replaceAll("%Killer%", k.getName());
-        }
-        return msg;
+    public static String playerPH(String msg, Player p) {
+        return msg.replaceAll("%player%", p.getName());
+    }
+    public static String killerPH(String msg, Player p) {
+        return msg.replaceAll("%killer%", p.getName());
+    }
+    public static String streakPH(String msg, Player p) {
+        return msg.replaceAll("%steak%", Integer.toString(DataController.getSteak(p)));
     }
 
     public static int rand (int max, int min) {
